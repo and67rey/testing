85377f44bd18a5b1d4ef5888a242afd4d2057ad7
Andrey <adulub@spinupper.com>
2025-05-05 06:44:19 -0400

    v 1.0

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+from main import remainder
+
+class TestDivide(unittest.TestCase):
+    def test_remainder_success(self):
+        self.assertEqual(remainder(10, 2), 0)
+        self.assertEqual(remainder(23, 5), 3)
+        self.assertEqual(remainder(17, -3), -1)
+        self.assertEqual(remainder(-9, 2), 1)
+
+
+    def test_remainder_by_zero(self):
+        self.assertRaises(ValueError, remainder, 7, 0)
+        self.assertRaises(ValueError, remainder, -11, 0)
+
+
+    if __name__ == '__main__':
+        unittest.main()
