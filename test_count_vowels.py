4d04fc08b24d19b49bc242fa8a83bcd96f9e8472
Andrey <adulub@spinupper.com>
2025-05-08 03:49:14 -0400

    v 1.1

--- test_count_vowels.py
+++ test_count_vowels.py
@@ -4,15 +4,22 @@ from AT02 import count_vowels
 @pytest.mark.parametrize("test_input,expected", [
     ('апрель', 2),
     ('Антарес', 3),
+    ('эффект наблюдателя', 7),
+    ('Осип охрип, Архип осип', 8),
     ('Python', 2),
     ('Orion', 3),
+    ('East or West home is best', 8),
     ('АоУЕ', 4),
     ('uEaiO', 5),
+    ('оУАиюЯ EuoAI', 11),
     ('йЦКнГшЩз', 0),
     ('QwrTPsDFg', 0),
+    ('zXDrnGtPKmq цНгКтРМЧж', 0),
     ('', 0),
     ('12345', 0),
-    ('~!@#$%^&*()_-+=|:;/.,', 0),
+    ('Лаланд 21185', 2),
+    ('TRAPPIST-1', 2),
+    ('~!@#$% ^&*()_- +=| :;/.,', 0),
 ])
 def test_count_vowels(test_input, expected):
     assert count_vowels(test_input) == expected
